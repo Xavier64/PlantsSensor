33d36e9de4961e2fbe17d4329915a85bb9ee6654
Xavier64 <xavierpincon@gmail.com>
2023-05-04 09:36:34 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './intranet/home/home.component';
+import { PlantsComponent } from './intranet/plants/plants.component';
+import { HumidityComponent } from './intranet/humidity/humidity.component';
+import { SensorComponent } from './intranet/sensor/sensor.component';
+import { AlertComponent } from './intranet/alert/alert.component';
+import { HistoricComponent } from './intranet/historic/historic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlantsComponent,
+    HumidityComponent,
+    SensorComponent,
+    AlertComponent,
+    HistoricComponent
   ],
   imports: [
     BrowserModule,
